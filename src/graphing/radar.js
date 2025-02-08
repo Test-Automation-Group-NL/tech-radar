926d06084e3e3adaa9cc741b8f4d0deb7842daca
wswebcreation <wim@wswebcreation.nl>
2025-02-08 15:20:41 +0100

    feat: add print button back
    - fix styling

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -633,13 +633,13 @@ const Radar = function (size, radar) {
       addButton(quadrants[i])
     })
 
-    // buttonsGroup
-    //   .append('div')
-    //   .classed('print-radar-btn', true)
-    //   .append('div')
-    //   .classed('print-radar button no-capitalize', true)
-    //   .text('Print this radar')
-    //   .on('click', window.print.bind(window))
+    buttonsGroup
+      .append('div')
+      .classed('print-radar-btn', true)
+      .append('div')
+      .classed('print-radar button no-capitalize', true)
+      .text('Print this radar')
+      .on('click', window.print.bind(window))
 
     alternativeDiv
       .append('div')
